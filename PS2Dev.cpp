68fcff2f82330b6775cbaf0885e50d3c3565ef29
liumazi <71892967@qq.com>
2020-03-17 21:32:07 +0800

    update to ps2dev 0.0.2

--- PS2Dev.cpp
+++ PS2Dev.cpp
@@ -2,37 +2,42 @@
  * ps2dev.cpp - an interface library for ps2 host.
  * limitations:
  *      we do not handle parity errors.
- *      The timing constants are hard coded from the spec. Data rate is not impressive.
+ *      The timing constants are hard coded from the spec. Data rate is
+ *         not impressive.
  *      probably lots of room for optimization.
  */
 
-//#include "WProgram.h"
 #include "ps2dev.h"
 
-// since for the device side we are going to be in charge of the clock,
-// the two defines below are how long each _phase_ of the clock cycle is
-#define CLKFULL 40
+//Enable serial debug mode?
+//#define _PS2DBG Serial
 
+//since for the device side we are going to be in charge of the clock,
+//the two defines below are how long each _phase_ of the clock cycle is
+#define CLKFULL 40
 // we make changes in the middle of a phase, this how long from the
 // start of phase to the when we drive the data line
 #define CLKHALF 20
+
+// Delay between bytes
+// I've found i need at least 400us to get this working at all,
+// but even more is needed for reliability, so i've put 1000us
+#define BYTEWAIT 1000
+
+// Timeout if computer not sending for 30ms
+#define TIMEOUT 30
+
 
 /*
  * the clock and data pins can be wired directly to the clk and data pins
  * of the PS2 connector.  No external parts are needed.
  */
-ps2dev::ps2dev(int clk_pin, int data_pin) :
-_clk_pin(clk_pin),
-_data_pin(data_pin)
+PS2dev::PS2dev(int clk, int data)
 {
-#ifdef DEBUG
-	Serial.print("ps2dev ");
-	Serial.print(clk_pin, HEX);
-	Serial.print(" ");
-	Serial.println(data_pin, HEX);
-#endif
-	set_high(_clk_pin); // release pins
-	set_high(_data_pin);
+  _ps2clk = clk;
+  _ps2data = data;
+  gohi(_ps2clk);
+  gohi(_ps2data);
 }
 
 /*
@@ -41,206 +46,270 @@ _data_pin(data_pin)
  * various conditions.  It's done this way so you don't need
  * pullup resistors.
  */
-void ps2dev::set_high(int pin)
+void
+PS2dev::gohi(int pin)
 {
-	pinMode(pin, INPUT);
-	digitalWrite(pin, HIGH);
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH);
 }
 
-void ps2dev::set_low(int pin)
+void
+PS2dev::golo(int pin)
 {
-	pinMode(pin, OUTPUT);
-	digitalWrite(pin, LOW);
+  digitalWrite(pin, LOW);
+  pinMode(pin, OUTPUT);
 }
 
-int ps2dev::write_byte(unsigned char data)
+int PS2dev::write(unsigned char data)
 {
-	unsigned char i;
-	unsigned char parity = 1; // odd parity
+  delayMicroseconds(BYTEWAIT);
 
-#ifdef DEBUG
-	Serial.print("write ");
-	Serial.println(data, HEX);
+  unsigned char i;
+  unsigned char parity = 1;
+
+#ifdef _PS2DBG
+  _PS2DBG.print(F("sending "));
+  _PS2DBG.println(data,HEX);
 #endif
 
-	if (digitalRead(_clk_pin) == LOW)
-	{
-#ifdef DEBUG
-		Serial.println("write is inhibited by clk pin");
+  if (digitalRead(_ps2clk) == LOW) {
+    return -1;
+  }
+
+  if (digitalRead(_ps2data) == LOW) {
+    return -2;
+  }
+
+  golo(_ps2data);
+  delayMicroseconds(CLKHALF);
+  // device sends on falling clock
+  golo(_ps2clk);	// start bit
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
+
+  for (i=0; i < 8; i++)
+    {
+      if (data & 0x01)
+      {
+        gohi(_ps2data);
+      } else {
+        golo(_ps2data);
+      }
+      delayMicroseconds(CLKHALF);
+      golo(_ps2clk);
+      delayMicroseconds(CLKFULL);
+      gohi(_ps2clk);
+      delayMicroseconds(CLKHALF);
+
+      parity = parity ^ (data & 0x01);
+      data = data >> 1;
+    }
+  // parity bit
+  if (parity)
+  {
+    gohi(_ps2data);
+  } else {
+    golo(_ps2data);
+  }
+  delayMicroseconds(CLKHALF);
+  golo(_ps2clk);
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
+
+  // stop bit
+  gohi(_ps2data);
+  delayMicroseconds(CLKHALF);
+  golo(_ps2clk);
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
+
+  delayMicroseconds(BYTEWAIT);
+
+#ifdef _PS2DBG
+  _PS2DBG.print(F("sent "));
+  _PS2DBG.println(data,HEX);
 #endif
-		return 1;
-	}
 
-	if (digitalRead(_data_pin) == LOW)
-	{
-#ifdef DEBUG
-		Serial.println("write is inhibited by data pin");
-#endif
-		return 2;
-	}
-
-	// device sends on falling clk
-	// delayMicroseconds(CLKHALF); ??
-
-	// start bit
-	set_low(_data_pin); // = 0
-	delayMicroseconds(CLKHALF); // data changed, 5-25us delay at least
-	set_low(_clk_pin); // falling clk, host will latch data
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin); //  rising clk, ready next falling clk
-	delayMicroseconds(CLKHALF); // rising clk to change data, 5us delay at least
-
-	for (i = 0; i < 8; i++)
-	{
-		if (data & 0x01) // bit i of data
-		{
-			set_high(_data_pin);
-		}
-		else
-		{
-			set_low(_data_pin);
-		}
-		delayMicroseconds(CLKHALF); // data changed, 5-25us delay at least
-		set_low(_clk_pin);	// falling clk, host will latch data
-		delayMicroseconds(CLKFULL);
-		set_high(_clk_pin); // rising clk, ready next falling clk
-		delayMicroseconds(CLKHALF); // rising clk to change data, 5us delay at least
-
-		parity = parity ^ (data & 0x01);
-		data = data >> 1;
-	}
-
-	// parity bit
-	if (parity)
-	{
-		set_high(_data_pin);
-	}
-	else
-	{
-		set_low(_data_pin);
-	}
-	delayMicroseconds(CLKHALF);
-	set_low(_clk_pin);
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin);
-	delayMicroseconds(CLKHALF);
-
-	// stop bit
-	set_high(_data_pin); // = 1
-	delayMicroseconds(CLKHALF);
-	set_low(_clk_pin);
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin);
-	delayMicroseconds(CLKHALF);
-
-	delayMicroseconds(50); // =50us from the clk is released in sending the stop bit
-	return 0;
+  return 0;
 }
 
-int ps2dev::read_byte(unsigned char * ptr)
+int PS2dev::available() {
+  //delayMicroseconds(BYTEWAIT);
+  //return ( (digitalRead(_ps2data) == LOW) || (digitalRead(_ps2clk) == LOW) );
+  return ( (digitalRead(_ps2clk) == LOW) );
+}
+
+int PS2dev::read(unsigned char * value)
 {
-	unsigned char data = 0x00;
-	unsigned char i;
-	unsigned char bit = 0x01;
+  unsigned int data = 0x00;
+  unsigned int bit = 0x01;
 
-	unsigned char parity = 1;
+  unsigned char calculated_parity = 1;
+  unsigned char received_parity = 0;
 
-	int ret = 0;
+  //wait for data line to go low and clock line to go high (or timeout)
+  unsigned long waiting_since = millis();
+  while((digitalRead(_ps2data) != LOW) || (digitalRead(_ps2clk) != HIGH)) {
+    if((millis() - waiting_since) > TIMEOUT) return -1;
+  }
 
-	// the host inhibit communication by pulling clk low for at least 100 microseconds.
-	// the host apply "request-to-send" by pulling data low, then release Clock.
+  delayMicroseconds(CLKHALF);
+  golo(_ps2clk);
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
 
-	// wait for data line to go low (request-to-send)
-	while (digitalRead(_data_pin) == HIGH) // start bit == 0 ??
-	{
-	}
-	// wait for clk line to go high (inhibit communication end)
-	while (digitalRead(_clk_pin) == LOW)
-	{
-	}
-	// the time it takes the device to begin generating clock pulses after
-	// the host initially takes the clock line low, must be no greater than 15ms 
+  while (bit < 0x0100) {
+    if (digitalRead(_ps2data) == HIGH)
+      {
+        data = data | bit;
+        calculated_parity = calculated_parity ^ 1;
+      } else {
+        calculated_parity = calculated_parity ^ 0;
+      }
 
-	// where is the start bit ??
+    bit = bit << 1;
 
-	// ready read bit 1 of data
-	delayMicroseconds(CLKHALF); // must be no greater than 15ms
-	set_low(_clk_pin); // falling clk, host waiting for (to set the bit 1 of data)
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin); // rising clk, device will latch data, and ready next falling clk
-	delayMicroseconds(CLKHALF);
+    delayMicroseconds(CLKHALF);
+    golo(_ps2clk);
+    delayMicroseconds(CLKFULL);
+    gohi(_ps2clk);
+    delayMicroseconds(CLKHALF);
 
-	for (i = 0; i < 8; i++)
-	{
-		if (digitalRead(_data_pin) == HIGH)
-		{
-			data = data | bit;
-		}
-		else
-		{
-		}
+  }
+  // we do the delay at the end of the loop, so at this point we have
+  // already done the delay for the parity bit
 
-		bit = bit << 1;
+  // parity bit
+  if (digitalRead(_ps2data) == HIGH)
+    {
+      received_parity = 1;
+    }
 
-		delayMicroseconds(CLKHALF);
-		set_low(_clk_pin);	// falling clk, host waiting for (to set the next bit)
-		delayMicroseconds(CLKFULL);
-		set_high(_clk_pin); // rising clk, device will latch data, and ready next falling clk
-		delayMicroseconds(CLKHALF);
+  // stop bit
+  delayMicroseconds(CLKHALF);
+  golo(_ps2clk);
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
 
-		parity = parity ^ (data & 0x01);
-	}
-	// we do the delay at the end of the loop, so at this point we have
-	// already done the delay for the parity bit
-	if (digitalRead(_data_pin) != parity)
-	{
-#ifdef DEBUG
-		Serial.println("parity bit error");
+
+  delayMicroseconds(CLKHALF);
+  golo(_ps2data);
+  golo(_ps2clk);
+  delayMicroseconds(CLKFULL);
+  gohi(_ps2clk);
+  delayMicroseconds(CLKHALF);
+  gohi(_ps2data);
+
+
+  *value = data & 0x00FF;
+
+#ifdef _PS2DBG
+  _PS2DBG.print(F("received data "));
+  _PS2DBG.println(*value,HEX);
+  _PS2DBG.print(F("received parity "));
+  _PS2DBG.print(received_parity,BIN);
+  _PS2DBG.print(F(" calculated parity "));
+  _PS2DBG.println(received_parity,BIN);
 #endif
-		ret |= 1;
-	}
+  if (received_parity == calculated_parity) {
+    return 0;
+  } else {
+    return -2;
+  }
 
-	// stop bit
-	delayMicroseconds(CLKHALF);
-	set_low(_clk_pin);	// falling clk, host waiting for (to set the stop bit)
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin); // rising clk, device will latch stop bit, and ready next falling clk
-	delayMicroseconds(CLKHALF);
-	if (digitalRead(_data_pin) != 1)
-	{
-#ifdef DEBUG
-		Serial.println("stop bit error");
+}
+
+
+void PS2dev::keyboard_init()
+{
+  while(write(0xAA)!=0);
+  delay(10);
+  return;
+}
+
+void PS2dev::ack()
+{
+  while(write(0xFA));
+  return;
+}
+
+int PS2dev::keyboard_reply(unsigned char cmd, unsigned char *leds)
+{
+  unsigned char val;
+  unsigned char enabled;
+  switch (cmd)
+  {
+  case 0xFF: //reset
+    ack();
+    //the while loop lets us wait for the host to be ready
+    while(write(0xAA)!=0);
+    break;
+  case 0xFE: //resend
+    ack();
+    break;
+  case 0xF6: //set defaults
+    //enter stream mode
+    ack();
+    break;
+  case 0xF5: //disable data reporting
+    //FM
+    enabled = 0;
+    ack();
+    break;
+  case 0xF4: //enable data reporting
+    //FM
+    enabled = 1;
+    ack();
+    break;
+  case 0xF3: //set typematic rate
+    ack();
+    if(!read(&val)) ack(); //do nothing with the rate
+    break;
+  case 0xF2: //get device id
+    ack();
+    write(0xAB);
+    write(0x83);
+    break;
+  case 0xF0: //set scan code set
+    ack();
+    if(!read(&val)) ack(); //do nothing with the rate
+    break;
+  case 0xEE: //echo
+    //ack();
+    write(0xEE);
+    break;
+  case 0xED: //set/reset LEDs
+    ack();
+    if(!read(leds)) ack(); //do nothing with the rate
+#ifdef _PS2DBG
+    _PS2DBG.print("LEDs: ");
+    _PS2DBG.println(*leds, HEX);
+    //digitalWrite(LED_BUILTIN, *leds);
 #endif
-		ret |= 2;
-	}
+    return 1;
+    break;
+  }
+  return 0;
+}
 
-	// acknowledge bit
-	delayMicroseconds(15); // rising clk end
-	set_low(_data_pin); // host wating for device pulling data
-	delayMicroseconds(5);
-	set_low(_clk_pin);	 // host wating for device pulling clk
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin);  // host wating for device release clk
-	delayMicroseconds(5);
-	set_high(_data_pin); // host wating for device release data
-	/*
-	delayMicroseconds(CLKHALF); // rising clk end
-	set_low(_data_pin); // host wating for device pulling data
-	set_low(_clk_pin);  // host wating for device pulling clk
-	delayMicroseconds(CLKFULL);
-	set_high(_clk_pin);  // host wating for device release clk
-	delayMicroseconds(CLKHALF);
-	set_high(_data_pin); // host wating for device release data
-	*/
+int PS2dev::keyboard_handle(unsigned char *leds) {
+  unsigned char c;  //char stores data recieved from computer for KBD
+  if(available())
+  {
+    if(!read(&c)) return keyboard_reply(c, leds);
+  }
+  return 0;
+}
 
-	// delay 45 microseconds (to give host time to inhibit next transmission) == acknowledge bit ??
-	// delayMicroseconds(45); ??
-
-#ifdef DEBUG
-	Serial.print("received ");
-	Serial.println(data, HEX);
-#endif
-
-	*ptr = data;
-	return ret;
+int PS2dev::keyboard_mkbrk(unsigned char code)
+{
+  write(code);
+  write(0xF0);
+  write(code);
+  return 0;
 }
--- PS2Dev.h
+++ PS2Dev.h
@@ -2,32 +2,33 @@
  * ps2dev.h - a library to interface with ps2 hosts. See comments in
  * ps2.cpp.
  * Written by Chris J. Kiick, January 2008.
- * Modified by Gene E. Scogin, August 2008.
- * Modified by LiuLiu.mz, March 2020.
+ * modified by Gene E. Scogin, August 2008.
  * Release into public domain.
  */
 
 #ifndef ps2dev_h
 #define ps2dev_h
 
-#include "MacroDef.h"
 #include "Arduino.h"
 
-class ps2dev
+class PS2dev
 {
-public:
-	ps2dev(int clk_pin, int data_pin);
-
-protected:
-	int write_byte(unsigned char data); // return 0 == no error
-	int read_byte(unsigned char * ptr); // return 0 == no error
-
-private:
-	void set_high(int pin); // pin to high level
-	void set_low(int pin); // pin to low level
-
-	int _clk_pin;
-	int _data_pin;
+	public:
+		PS2dev(int clk, int data);
+		int write(unsigned char data);
+		int read(unsigned char * data);
+		int available();
+		void keyboard_init();
+		int keyboard_reply(unsigned char cmd, unsigned char *leds);
+		int keyboard_handle(unsigned char *leds);
+		int keyboard_mkbrk(unsigned char code);
+	private:
+		int _ps2clk;
+		int _ps2data;
+		void golo(int pin);
+		void gohi(int pin);
+		void ack();
 };
 
 #endif /* ps2dev_h */
+
