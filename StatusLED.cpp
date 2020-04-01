bc71e3a3966a03e231e7b3a7b4df6d7a2f9ee930
liumazi <71892967@qq.com>
2020-04-01 19:40:57 +0800

    add StatusLED

---
+++ StatusLED.cpp
@@ -0,0 +1,24 @@
+/*
+ * StatusLED.cpp - Show device working status by LED
+ *
+ * Written by LiuLiu.mz, April 2020.
+ *
+ */
+
+#include "StatusLED.h"
+
+StatusLED::StatusLED():
+
+{
+
+}
+
+void StatusLED::setup()
+{
+
+}
+
+void StatusLED::loop()
+{
+
+}
---
+++ StatusLED.h
@@ -0,0 +1,23 @@
+/*
+ * StatusLED.h - Show device working status by LED
+ *
+ * Written by LiuLiu.mz, April 2020.
+ *
+ */
+
+#ifndef _STATUS_LED_H_
+#define _STATUS_LED_H_
+
+class StatusLED
+{
+public:
+	StatusLED();
+
+	void setup();
+	void loop();
+
+private:
+
+};
+
+#endif //_STATUS_LED_H_
